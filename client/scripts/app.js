910808a964bffb7921e2b84041265f098027ada4
pavelmp <pavelmp@gmail.com>
2016-01-25 15:08:30 -0800

    pass basic specs

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,2 +1,61 @@
 // YOUR CODE HERE:
+var app = {};
+app.server = 'https://api.parse.com/1/classes/chatterbox';
 
+app.init = function () {
+
+};
+
+app.send = function (message) {
+  $.ajax({
+    url: app.server,
+    type: 'POST',
+    data: JSON.stringify(message),
+    contentType: 'application/json',
+    success: function (data) {
+      console.log('chatterbox: Message sent');
+    },
+    error: function (data) {
+      console.error('chatterbox: Failed to send message');
+    }
+  });
+};
+
+app.fetch = function () {
+  $.ajax({
+    url: app.server,
+    type: 'GET',
+    contentType: 'application/json',
+    success: function (data) {
+      console.log('chatterbox: Message sent');
+    },
+    error: function (data) {
+      console.error('chatterbox: Failed to send message');
+    }
+  });
+};
+
+app.clearMessages = function() {
+  $('#chats').empty();
+};
+
+app.addMessage = function(message) {
+  var $node = $('<p>' + message.username + ': ' + message.text + '</p>');
+  $node.appendTo('#chats');
+};
+
+app.addRoom = function(room) {
+  var $node = $('<option value=' + '"' + room + '">' + room + '</option>');
+  $('#roomSelect').append($node);
+};
+
+app.addFriend = function(friend){
+};
+
+app.handleSubmit = function(){
+};
+// var message = {
+//   username: 'shawndrost',
+//   text: 'trololo',
+//   roomname: '4chan'
+// };
