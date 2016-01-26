6d604b6bf6be51f4ff32cc92029691913bb034c6
pavelmp <pavelmp@gmail.com>
2016-01-25 16:54:27 -0800

    display messages from various rooms

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -2,6 +2,9 @@
 var app = {};
 app.server = 'https://api.parse.com/1/classes/chatterbox';
 
+app.messages = [];
+app.rooms = [];
+
 app.init = function () {
 
 };
@@ -27,7 +30,8 @@ app.fetch = function () {
     type: 'GET',
     contentType: 'application/json',
     success: function (data) {
-      console.log('chatterbox: Message sent');
+      // console.log(data);
+      app.getMessages(data);
     },
     error: function (data) {
       console.error('chatterbox: Failed to send message');
@@ -35,12 +39,50 @@ app.fetch = function () {
   });
 };
 
+app.getMessages = function(data){
+  app.messages = data.results.filter(function(message,i,a){return !!message.username && !!message.text && !!message.roomname;});
+  app.rooms = app.messages.map(function(message){return message.roomname;})
+                          .filter(function(room,index,arr){return arr.indexOf(room) === index;});
+  app.rooms.forEach(function(room){
+    var $node = '<option value=' + '"' + room + '">' + room + '</option>';
+    $('#roomSelect').append($node);
+  });
+  app.updateRoom();
+};
+
+app.updateRoom = function(){
+  var curRoom = $('#roomSelect option:selected').text();  
+  app.clearMessages();
+  app.messages.filter(function (v, i, a) {return v.roomname === curRoom;})
+              .forEach(function (message) {app.addMessage(message);});
+};
+
+app.fetch();
+
 app.clearMessages = function() {
   $('#chats').empty();
 };
 
+
+app.escape = function(string){
+  string  = string.replace(/\&/g, function (v) {
+    return '&amp';
+  }).replace(/\</g, function (v) {
+    return '&lt';
+  }).replace(/\>/g, function (v) {
+    return '&gt';
+  }).replace(/\"/g, function (v) {
+    return '&quot';
+  }).replace(/\'/g, function (v) {
+    return '&#x27';
+  }).replace(/\//g, function (v) {
+    return '&#x2F';
+  });
+  return string;
+};
+
 app.addMessage = function(message) {
-  var $node = $('<p>' + message.username + ': ' + message.text + '</p>');
+  var $node = $('<div class="chat" data-username=' + '"' + app.escape(message.username) + '" data-room="' + app.escape(message.roomname) + '">' + app.escape(message.username) + ': ' + app.escape(message.text) + '</div>');
   $node.appendTo('#chats');
 };
 
