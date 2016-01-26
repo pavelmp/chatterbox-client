1243f3a3e875662e85896137b066bfa5b1270f02
pavelmp <pavelmp@gmail.com>
2016-01-25 20:44:20 -0800

    allows user to submit messges

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -5,23 +5,19 @@ app.server = 'https://api.parse.com/1/classes/chatterbox';
 app.messages = [];
 app.rooms = [];
 app.friends = [];
+app.username = '';
 
 app.init = function () {
-
 };
 
 $(document).on("click", ".username", function () {
   var friend = $(this).text();
-  console.log(friend);
-  if($('.' + friend).hasClass("friend")){
-    app.friends.splice(app.friends.indexOf(friend),1);
-    $('.' + friend).removeClass("friend");
-  } else {
-    if(app.friends.indexOf(friend) === -1){
-      app.friends.push(friend);
-    }
-    $('.' + friend).addClass("friend");
-  }    
+  app.addFriend(friend);
+});
+
+$(document).on("click", "#submit", function () {
+  var newMessage = $('#message').val();
+  app.handleSubmit(newMessage);
 });
 
 app.send = function (message) {
@@ -43,9 +39,10 @@ app.fetch = function () {
   $.ajax({
     url: app.server,
     type: 'GET',
+    data: {"order": "-createdAt"},
     contentType: 'application/json',
     success: function (data) {
-      // console.log(data);
+      console.log(data);
       app.getMessages(data);
     },
     error: function (data) {
@@ -58,6 +55,8 @@ app.getMessages = function(data){
   app.messages = data.results.filter(function(message,i,a){return !!message.username && !!message.text && !!message.roomname;});
   app.rooms = app.messages.map(function(message){return message.roomname;})
                           .filter(function(room,index,arr){return arr.indexOf(room) === index;});
+  
+  $('#roomSelect').empty();                        
   app.rooms.forEach(function(room){
     var $node = '<option value=' + '"' + room + '">' + room + '</option>';
     $('#roomSelect').append($node);
@@ -68,6 +67,7 @@ app.getMessages = function(data){
 app.updateRoom = function(){
   var curRoom = $('#roomSelect option:selected').text();  
   app.clearMessages();
+  //console.log(app.messages);
   app.messages.filter(function (v, i, a) {return v.roomname === curRoom;})
               .forEach(function (message) {app.addMessage(message);});
 };
@@ -113,10 +113,30 @@ app.addRoom = function(room) {
 };
 
 app.addFriend = function(friend){
-
+  if($('.' + friend).hasClass("friend")){
+    app.friends.splice(app.friends.indexOf(friend),1);
+    $('.' + friend).removeClass("friend");
+  } else {
+    if(app.friends.indexOf(friend) === -1){
+      app.friends.push(friend);
+    }
+    $('.' + friend).addClass("friend");
+  }   
 };
 
-app.handleSubmit = function(){
+app.getUserName = function(){
+  var results = new RegExp('[\?&]username=([^&#]*)').exec(window.location.href);
+  return results[1] || 0;  
+};
+
+
+app.handleSubmit = function(message){
+  var messageObj = {
+    username: app.getUserName(),
+    text: message,
+    roomname: $('#roomSelect option:selected').text()
+  };
+  app.send(messageObj);
 };
 // var message = {
 //   username: 'shawndrost',
